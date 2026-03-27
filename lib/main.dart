c1dbd375d174cb6d41975cf61b562c8457365863
XZaid122 <79021113+XZaid122@users.noreply.github.com>
2026-03-27 18:18:34 -0500

    Refactor main.dart to use const and custom button

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Unitecnar',
       home: Scaffold(
-        appBar: AppBar(title: Text('Unitecnar 5.0'),
-        
+        appBar: AppBar(
+          title: const Text('Unitecnar 5.0'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('Hola Mundo, Hola Werachamo'),
         ),
-        floatingActionButton:  
-        FloatingActionButton(
-          onPressed: (()=>{}),
-          child: Icon(Icons.plus_one)
-          ),
+        floatingActionButton: _CustomButton(
+          icon: Icons.add,
+          onPressed: () {
+            print("Botón");
+          },
+        ),
       ),
-      );
+    );
+  }
+}
+
+class _CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const _CustomButton({
+    required this.icon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
   }
 }
